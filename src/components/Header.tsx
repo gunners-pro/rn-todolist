94c660ffdd1b01731a735771237b00b284f38103
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-20 19:08:48 -0300

    style: add logo animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Center, VStack } from 'native-base';
+import Lottie from 'lottie-react-native';
 
 import Logo from '../assets/logo.svg';
+import Rocket from '../assets/rocket.json';
 
 export function Header() {
   return (
     <VStack h="250" bg="gray.700">
-      <Center h="full">
-        <Logo height={200} width={150} />
+      <Center h="full" flexDir="row">
+        <Lottie
+          source={Rocket}
+          autoPlay
+          loop
+          style={{ width: 60, height: 60 }}
+        />
+        <Logo height={120} width={120} />
       </Center>
     </VStack>
   );
